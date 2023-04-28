d0c5156bebdf4912fa47840212344ff5a2dd8d36
hjoonpark <hjoonpark.us@gmail.com>
2023-04-28 01:56:28 -0500

    sync2

--- modules/models/utils.py
+++ modules/models/utils.py
@@ -15,11 +15,11 @@ class DoubleConv(nn.Module):
             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(mid_channels),
             nn.ReLU(inplace=True),
-            nn.Dropout(0.2),
+            nn.Dropout(0.5),
             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
-            nn.Dropout(0.2),
+            nn.Dropout(0.5),
         )
 
     def forward(self, x):
